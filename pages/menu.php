b04b9093080cc3296cbd5c13da472e55cee3d879
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2020-12-29 17:01:26 +0200

    Menu active section

--- pages/menu.php
+++ pages/menu.php
@@ -1,3 +1,4 @@
+<?php $page = $_GET['page'];?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">Gallery site</a>
@@ -7,16 +8,16 @@
             <div id="navbarNav" class="collapse navbar-collapse">
                 <ul class="navbar-nav">
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="index.php?page=1">Home</a>
+                        <a class="<?php if ($page=="1") {echo "active"; } else {echo " ";}?> nav-link" href="index.php?page=1">Home</a>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="index.php?page=2">Upload</a>
+                        <a class=" <?php if ($page=="2") {echo "active"; } else {echo " ";}?> nav-link" href="index.php?page=2">Upload</a>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="index.php?page=3">Gallery</a>
+                        <a class=" <?php if ($page=="3") {echo "active"; } else {echo " ";}?> nav-link" href="index.php?page=3">Gallery</a>
                     </li>
                     <li class="nav-item" role="presentation">
-                        <a class="nav-link" href="index.php?page=4">Registration</a>
+                        <a class=" <?php if ($page=="4") {echo "active"; } else {echo " ";}?> nav-link" href="index.php?page=4">Registration</a>
                     </li>
                 </ul>
             </div>
