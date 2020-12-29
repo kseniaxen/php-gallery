de6eb612dbd33e5ce73050cfab983f710321608f
Ksenia Serdiuk <serdiuk.xen@gmail.com>
2020-12-29 14:27:04 +0200

    init

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Gallery</title>
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+    <div class="container">
+        <div class="row">
+        <header class="col-sm-12 col-md-12 col-lg-12">
+        
+        </header>
+        </div>
+
+        <div class="row">
+        <nav class="col-sm-12 col-md-12 col-lg-12">
+        
+        </nav>
+        </div>
+
+        <div class="row">
+        <section class="col-sm-12 col-md-12 col-lg-12">
+
+        </section>
+        </div>
+        
+    </div>
+
+    <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
